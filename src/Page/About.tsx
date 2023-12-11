e454b54e1a794f3e989107b45ea096d7926d4f64
LilBoii <choreiwken@outlook.com>
2023-12-11 20:58:28 +0700

    Add Link & CSS to Button

--- src/Page/About.tsx
+++ src/Page/About.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const About: React.FC = () => {
   return (
@@ -13,18 +14,19 @@ const About: React.FC = () => {
               </div>
               <div className="margin-about-content">
                 <p>
-                  กำลังอยากคุยกับใครสักคนหรือไม่ เป็นห่วงบอทพร้อมจะเป็นเพื่อนรับฟังคุณเสมอ
+                  กำลังอยากคุยกับใครสักคนหรือไม่
+                  เป็นห่วงบอทพร้อมจะเป็นเพื่อนรับฟังคุณเสมอ
                 </p>
-                <button>แชทเลย!</button>
+                <Link to="/">
+                  <button className="chat-button">แชทเลย!</button>
+                </Link>
                 <div>
                   <hr className="header-line" />
-                  </div>
+                </div>
               </div>
             </div>
           </div>
         </div>
-
-
       </div>
     </div>
   );
